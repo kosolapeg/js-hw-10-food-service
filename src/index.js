e385ae8fb43cd5e6ebb4ed9251232652d94d4aee
Grigor Bitny <kosolapeg@gmail.com>
2021-02-20 10:20:13 +0100

    Done!

--- src/index.js
+++ src/index.js
@@ -1,20 +1,47 @@
 import './sass/styles.scss';
 import menuTemplate from './templates/menu.hbs';
-// import menu from './menu.json';
+import storage from './modules/storage';
+import menu from './menu.json';
+
+const refs = {
+  menu: document.querySelector('.js-menu'),
+  togglerBtn: document.querySelector('#theme-switch-toggle'),
+  body: document.querySelector('body'),
+};
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-const markup = menuTemplate(menuData);
+const addInitialTheme = () => {
+  const savedTheme = storage.load('theme');
 
-const test = document.querySelector('#test');
+  if (savedTheme) {
+    refs.body.classList.add(savedTheme);
+    refs.togglerBtn.checked = savedTheme === Theme.DARK ? true : false;
+  } else {
+    refs.body.classList.add(Theme.LIGHT);
+    storage.save('theme', Theme.LIGHT);
+    savedTheme = Theme.LIGHT;
+  }
+};
 
-test.innerHTML = markup;
+const toggleBtnHandler = e => {
+  if (e.target.checked) {
+    refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
+    storage.save('theme', Theme.DARK);
+    e.target.checked = true;
+  } else {
+    refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    storage.save('theme', Theme.LIGHT);
+    e.target.checked = false;
+  }
+};
 
-console.log(storage.save);
+// Изначальная разметка меню
+const markup = menuTemplate(menu);
+refs.menu.insertAdjacentHTML('afterbegin', markup);
+addInitialTheme();
 
-storage.save('temVar', JSON.stringify(menuData));
-
-storage.remove('temVar');
+refs.togglerBtn.addEventListener('click', toggleBtnHandler);
